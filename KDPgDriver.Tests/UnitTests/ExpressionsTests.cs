75b5458df7561a252b8d3223957d6a440eebff1e
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-02-22 21:02:18 +0100

    extracted JsonTypesTests from ExpressionsTests

--- KDPgDriver.Tests/UnitTests/ExpressionsTests.cs
+++ KDPgDriver.Tests/UnitTests/ExpressionsTests.cs
@@ -348,36 +348,6 @@ namespace KDPgDriver.Tests.UnitTests
 
     // JSON
     [Fact]
-    public void ExpressionJsonString()
-    {
-      var val = JToken.FromObject("A");
-
-      var exp = NodeVisitor.VisitFuncExpression<MyModel>(x => x.JsonObject1["A"] == val);
-
-      Utils.AssertExpression(exp, @"((json_object1)->'A') = to_jsonb((@1::jsonb))",
-                             new Param("\"A\"", NpgsqlDbType.Jsonb));
-    }
-
-    [Fact]
-    public void ExpressionJsonNumber()
-    {
-      var val = JToken.FromObject(2);
-
-      var exp = NodeVisitor.VisitFuncExpression<MyModel>(x => x.JsonObject1["A"] == val);
-
-      Utils.AssertExpression(exp, @"((json_object1)->'A') = to_jsonb((@1::jsonb))",
-                             new Param("2", NpgsqlDbType.Jsonb));
-    }
-
-    [Fact]
-    public void ExpressionJsonModel()
-    {
-      var exp = NodeVisitor.VisitFuncExpression<MyModel>(x => x.JsonModel.Name);
-
-      Utils.AssertExpression(exp, @"(json_model->'name')::text");
-    }
-
-    [Fact]
     public void ExpressionJsonModelFunc()
     {
       var exp = NodeVisitor.VisitFuncExpression<MyModel>(x => x.JsonModel.Name.Substring(1, 2));
